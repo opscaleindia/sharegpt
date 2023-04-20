6a2da173d5324e2b6f1cf39f70310b0b89d1a661
madhur <madhuracharyaav@gmail.com>
2023-04-20 14:08:11 +0530

    Add redis container. Remove dependency of upstash. Add rate limiting. Update docker-cmpose local setup.

--- app/lib/upstash.ts
+++ app/lib/upstash.ts
@@ -2,7 +2,7 @@ import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
 // Initiate Redis instance
-export const redis = Redis.fromEnv();
+// export const redis = Redis.fromEnv();
 
 // Create a new ratelimiter, that allows 10 requests per 10 seconds
 export const ratelimit = new Ratelimit({
@@ -10,3 +10,15 @@ export const ratelimit = new Ratelimit({
   limiter: Ratelimit.slidingWindow(10, "10 s"),
   analytics: true,
 });
+
+
+import { createClient } from 'redis';
+
+export const redis= createClient({
+  url: process.env.REDIS_URL,
+  database: process.env.DATABASE_NAME,
+  // username: process.env.REDIS_USER,
+  password: process.env.REDIS_PASSWORD,
+});
+redis.on('error', err => console.log('Redis Client Error', err));
+redis.connect();
--- app/pages/api/logs/conversations.ts
+++ app/pages/api/logs/conversations.ts
@@ -1,24 +1,18 @@
-import { NextRequest } from "next/server";
+import { NextApiRequest, NextApiResponse } from "next";
 import { redis } from "@/lib/upstash";
 
-export const config = {
-  runtime: "experimental-edge",
-};
-
-export default async function handler(req: NextRequest) {
-  const search = req.nextUrl.searchParams.get("search");
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { search } = req.query as { search: string };
   if (!search) {
     return new Response("Invalid search param", { status: 400 });
   }
   if (req.method === "POST") {
-    const response = await redis.zincrby("searches", 1, search.toLowerCase());
-    return new Response(JSON.stringify(response), {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await redis.ZINCRBY("searches", 1, search.toLowerCase());
+    res.status(200).json(response);
   } else {
-    return new Response(`Method ${req.method} Not Allowed`, { status: 405 });
+    res.status(405).json({ error: "Method not allowed" });
   }
 }
