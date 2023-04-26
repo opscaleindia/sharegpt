e8ae3d82f65b8da90f933e7b2825f7bffda2eefa
madhur <madhuracharyaav@gmail.com>
2023-04-26 13:00:32 +0530

    Update gpt api and UI.

--- app/pages/api/generate.ts
+++ app/pages/api/generate.ts
@@ -17,24 +17,28 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid animal",
-      }
+  const gptQuery:string= req.body.trim();
+  if(gptQuery.length === 0) {
+    return res.status(400).json({
+      error: { message: "Query must not be empty"}
     });
-    return;
   }
 
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+      prompt: gptQuery,
+      temperature: 0.3,
+      max_tokens: 150,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+    res.status(200).json({ 
+      result: completion.data.choices[0].text,
+      raw: completion.data
+    });
+  } catch(error: any) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
@@ -50,14 +54,3 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
-}
--- app/pages/gpt.tsx
+++ app/pages/gpt.tsx
@@ -1,57 +1,46 @@
 import Head from "next/head";
-import { useState } from "react";
+import React, { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+  const [query, setQuery] = useState<string>("");
+  const [result, setResult] = useState<string|undefined>();
 
-  async function onSubmit(event) {
+  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ animal: animalInput }),
-      });
-
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
-      }
-
+    fetch("/api/generate", {
+      method: "POST",
+      body: query,
+    })
+    .then((res: Response) => res.json())
+    .then(data => {
       setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
-      // Consider implementing your own error handling logic here
-      console.error(error);
-      alert(error.message);
-    }
-  }
+      setQuery("");
+    })
+    .catch((err:any) => {
+      console.log(err);
+    });
+  };
 
   return (
     <div>
       <Head>
         <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>Chat with ChatGPT</h3>
         <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
+          <textarea
+            cols= {3}
+            name="query"
+            placeholder="Ask a question"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          ></textarea>
+          <input type="submit" value="Submit"/>
         </form>
-        <div className={styles.result}>{result}</div>
+        <pre className={styles.result}>{result}</pre>
       </main>
     </div>
   );
